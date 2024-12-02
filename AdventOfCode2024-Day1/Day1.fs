ceca9b42a98c1d2428973dfe73b6638823fea529
Ian Winter <ianskonto@googlemail.com>
2024-12-02 07:44:13 +0100

    day1

---
+++ AdventOfCode2024-Day1/Day1.fs
@@ -0,0 +1,40 @@
+﻿module Day1
+
+open System.IO
+
+let mapElem f (a, b) = (f a, f b)
+
+let spreaded f (a,b) = f a b  
+
+let zipWith f t =
+    t
+    |> spreaded Array.zip
+    |> Array.map (spreaded f)
+
+
+let parseLine (line:string) =
+    let splitLine = line.Replace("   ", "%").Split("%")
+    (int splitLine[0], int splitLine[1])
+
+
+let count xs x =
+    xs
+    |> Array.filter ((=) x)
+    |> Array.length
+
+
+let day1Result name = 
+    File.ReadAllLines(name) 
+    |> Array.map parseLine
+    |> Array.unzip
+    |> (mapElem Array.sort)
+    |> zipWith (-)
+    |> Array.map abs
+    |> Array.sum
+
+let day1Result2 name = 
+    File.ReadAllLines(name)
+    |> Array.map parseLine
+    |> Array.unzip
+    |> (fun (x,y) -> x |> Array.map (fun z -> count y z |> (*) z ))
+    |> Array.sum
--- AdventOfCode2024-Day1/Tests.fs
+++ AdventOfCode2024-Day1/Tests.fs
@@ -1,8 +1,20 @@
 module Tests
 
 open Xunit
-open AdventOfCode_Utils.Say
+open Day1
+
+let fileName = "Data1.txt"
+let exampleFileName = "Data1-Example.txt"
 
 [<Fact>]
 let ``My test`` () =
-    Assert.Equal("Hello Ian", hello("Ian"))
+    Assert.Equal(3246517, day1Result fileName)
+
+[<Fact>]
+let ``My test -ex`` () =
+    Assert.Equal(11, day1Result exampleFileName)
+
+
+[<Fact>]
+let ``My test2`` () =
+    Assert.Equal(29379307, day1Result2 fileName)
