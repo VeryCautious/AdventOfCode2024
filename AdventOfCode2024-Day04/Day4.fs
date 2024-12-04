8be62faddb2c420efc7f595b5aa88bbbaa2dcdf5
Ian Winter <ian.winter@medicalvalues.de>
2024-12-04 10:28:07 +0100

    day4 part 1

---
+++ AdventOfCode2024-Day04/Day4.fs
@@ -0,0 +1,58 @@
+﻿module Day4
+
+type Indicies = (int*int) array
+type Field = char array array
+
+let read2DArray fileName = 
+    System.IO.File.ReadAllLines(fileName)
+    |> Array.map (fun line -> line.ToCharArray())
+
+let getRowIndicies (array: Field) = 
+    let row = [|0..(array.Length - 1)|]
+    let col = [|0..(array.[0].Length - 1)|]
+    row
+    |> Array.map (fun x -> col |> Array.map (fun y -> (x, y)))
+
+let getSouthEastIndicies (array: Field) = 
+    let row = [|0..(array.Length - 1)|]
+    let col = [|0..(array.[0].Length - 1)|]
+    let upper = row |> Array.map (fun x -> col |> Array.map (fun y -> (x + y, y)))
+    let lower = [|1..(array.[0].Length - 1)|] |> Array.map (fun y -> row |> Array.map (fun x -> (x, x + y)))
+    Array.concat [|upper; lower|]
+    |> Array.map (Array.filter (fun (x, y) -> x >= 0 && x < array.Length && y >= 0 && y < array.[0].Length))
+
+let flipX (maxX: int) (ind: Indicies) = ind |> Array.map (fun (x, y) -> (maxX - x, y))
+let transpose (x,y) = (y,x)
+
+let getDiagonalIndicies (array: Field) = 
+    let southEast = getSouthEastIndicies array
+    let southWest = southEast |> Array.map (flipX (array.Length - 1))
+    Array.concat [|southEast; southWest|]
+
+let stringFrom (array: Field) (indicies: Indicies) = 
+    indicies |> Array.map (fun (x, y) -> array.[x].[y]) |> System.String
+
+let countWord (word: string) (hayStack:string) = 
+    [|0..(hayStack.Length - word.Length)|]
+    |> Array.map (fun x -> hayStack.Substring(x, word.Length))
+    |> Array.filter ((=) word)
+    |> Array.length
+
+let getPossibleIndecies (array: Field) = 
+    let rowIndicies = getRowIndicies array
+    let reversedRowIndicies = rowIndicies |> Array.map Array.rev
+    let colIndicies = getRowIndicies (Array.transpose array) |> Array.map (Array.map transpose)
+    let reversedColIndicies = colIndicies |> Array.map Array.rev
+    let diagonalIndicies = getDiagonalIndicies array
+    let reversedDiagonalIndicies = diagonalIndicies |> Array.map Array.rev
+    Array.concat [|rowIndicies; colIndicies; reversedRowIndicies; reversedColIndicies; diagonalIndicies; reversedDiagonalIndicies|]
+
+let countWordInArray (word: string) (array: Field) =
+    getPossibleIndecies array
+    |> Array.map (stringFrom array) 
+    |> Array.map (countWord word)
+    |> Array.sum
+
+let solvePart1 (fileName: string) = 
+    read2DArray fileName
+    |> countWordInArray "XMAS" 
---
+++ AdventOfCode2024-Day04/Tests.fs
@@ -0,0 +1,49 @@
+module Tests
+
+open Xunit
+open Day4
+
+let exampleFile = "Data4-Example.txt"
+let file = "Data4.txt"
+
+[<Fact>]
+let ``Part1 ExampleFile`` () =
+    Assert.Equal(18, solvePart1 exampleFile)
+
+[<Fact>]
+let ``Part1 File`` () =
+    Assert.Equal(2644, solvePart1 file)
+
+module IndexTests =
+
+    [<Fact>]
+    let ``2x2 Square -> getSouthEastIndicies -> 3 IndexSeries`` () =
+        Assert.Equal(3, getSouthEastIndicies [|[|'a';'a'|]; [|'a';'a'|]|] |> Array.length)
+
+    [<Fact>]
+    let ``3x3 Square -> getSouthEastIndicies -> All Present`` () =
+        let expected = [| [|(0,0); (1,1); (2,2)|] ; [|(1,0); (2,1)|] ; [|(2,0)|] ; [|(0,1); (1,2)|] ; [|(0,2)|] |]
+        let actual = getSouthEastIndicies [|[|'a';'a';'a'|]; [|'a';'a';'a'|]; [|'a';'a';'a'|]|]
+        Assert.Equal(5, expected.Length)
+        expected
+        |> Array.iter (fun x -> Assert.Contains(x, actual))
+
+    [<Fact>]
+    let ``2x3 Shape -> getSouthEastIndicies -> All Present`` () =
+        let field = [| [|'a';'a';'a'|]; [|'a';'a';'a'|] |]
+        let expected = [| [|(0,0); (1,1)|] ; [|(1,0)|] ; [|(0,1);(1,2)|] ; [|(0,2)|] |]
+        let actual = getSouthEastIndicies field
+        Assert.Equal(2, field.Length)
+        Assert.Equal(3, field.[0].Length)
+        Assert.Equal(4, actual.Length)
+        expected
+        |> Array.iter (fun x -> Assert.Contains(x, actual))
+
+    [<Fact>]
+    let ``2x3 Shape -> transpose -> transposed`` () =
+        let field = [| [|'a';'b';'c'|]; [|'d';'e';'f'|] |]
+        let expected = [| [|'a';'b'|]; [|'c';'d'|] ; [|'e';'f'|]|]
+        let actual = Array.transpose field
+        Assert.Equal(expected.Length, actual.Length)
+        Assert.Equal(expected.[0].Length, actual.[0].Length)
+        Assert.Equal(['a';'d'], actual[0])
