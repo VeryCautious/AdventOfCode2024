bc4ab9f4105d9f0a6f5eb380b3b567c54b9877de
Ian Winter <ian.winter@medicalvalues.de>
2024-12-07 15:11:18 +0100

    day7 part1

---
+++ AdventOfCode2024-Day07/Day7.fs
@@ -0,0 +1,52 @@
+﻿module Day7
+
+type Operation = Add | Mul
+
+let addOneMul (operations:Operation List) =
+    let n = operations.Length
+    [0..n-1]
+    |> List.filter (fun i -> operations[i] <> Mul)
+    |> List.map (fun i -> List.updateAt i Mul operations)
+
+let operatorCombinations (n:int) =
+    let rec combinations (n:int) (mMuls:int) =
+        if mMuls = 0 then
+            [[List.init n (fun _ -> Add)]]
+        else
+            let old = combinations n (mMuls-1)
+            let newComb = old |> List.last |> List.collect addOneMul |> List.distinct
+            old @ [newComb]
+    combinations n n
+
+let eval (values:int64 array) (operations:Operation list) =
+    let zip = Array.zip values (List.toArray (Add::operations))
+    zip
+    |> Array.fold (fun acc (v, o) -> 
+        match o with
+            | Add -> acc + v
+            | Mul -> acc * v
+        ) 0L
+
+let isSolvable (values: int64 array) (target: int64) =
+    operatorCombinations (values.Length-1)
+    |> List.map (List.map (fun ops -> (eval values ops, Array.contains 1L values)))
+    |> List.takeWhile (List.exists (fun (ev,hasOne) -> ev <= target || hasOne))
+    |> List.map (List.map fst)
+    |> List.exists (List.exists ((=) target))
+
+let parseLine (line:string) =
+    let split = line.Split(": ")
+    match split with
+        | [|target;values|] -> 
+            (int64 target, values.Split(' ') |> Array.map int64)
+        | _ -> failwith "Invalid input"
+
+let parseInput fileName =
+    System.IO.File.ReadAllLines(fileName)
+    |> Array.map parseLine
+
+let solvePart1 fileName =
+    parseInput fileName
+    |> Array.filter (fun (target, values) -> isSolvable values target)
+    |> Array.map fst
+    |> Array.sum
---
+++ AdventOfCode2024-Day07/Tests.fs
@@ -0,0 +1,42 @@
+module Tests
+
+open System
+open Xunit
+open Day7
+
+[<Fact>]
+let ``My test`` () =
+    Assert.Equal(3749L, solvePart1 "Data7-Example.txt")
+
+[<Fact>]
+let ``My testkmwalda`` () =
+    let value = solvePart1 "Data7.txt"
+    Assert.Equal(4122618559853L, value)
+
+[<Fact>]
+let ``adwMy testa`` () =
+    let x = addOneMul [Add;Add;Add]
+    Assert.Equal(3, x.Length)
+    Assert.Equal([|Mul;Add;Add|], x[0])
+    Assert.Equal([|Add;Mul;Add|], x[1])
+    Assert.Equal([|Add;Add;Mul|], x[2])
+
+[<Fact>]
+let ``aval`` () =
+    let value = eval [|2;3;4|] [Add;Mul]
+    Assert.Equal((2L+3L)*4L, value)
+
+[<Fact>]
+let ``avalawd`` () =
+    let value = eval [|11;6;16;20|] [Add;Mul;Add]
+    Assert.Equal(292L, value)
+
+[<Fact>]
+let ``avawawdwal`` () =
+    Assert.Equal(1, [[1;2];[1;2]] |> List.distinct |> List.length)
+
+[<Fact>]
+let ``avawdal`` () =
+    let value = operatorCombinations 6
+    Assert.Equal(64, value |> List.collect id |> List.distinct |> List.length)
+    value |> List.iteri (fun i b -> Assert.True(List.forall (fun a -> a |> List.filter ((=) Mul) |> List.length |> ((=) i)) b))
