48bf9537bfe51ea683a3ebfd71c0cf867e188a19
Ian Winter <ianskonto@googlemail.com>
2024-12-07 18:54:44 +0100

    refactor

--- AdventOfCode2024-Day07/Day7.fs
+++ AdventOfCode2024-Day07/Day7.fs
@@ -1,32 +1,12 @@
 ﻿module Day7
 
-open AdventOfCode_Utils
-
 type Operation = Add | Mul | Concat
 
-let addOne (op:Operation) (operations:Operation List) =
-    let n = operations.Length
-    [0..n-1]
-    |> List.filter (fun i -> operations[i] = Add)
-    |> List.map (fun i -> List.updateAt i op operations)
-let addOneMul = addOne Mul
 
-let operatorCombinations (n:int) =
-    let rec combinations (n:int) (mMuls:int) =
-        if mMuls = 0 then
-            [[List.init n (fun _ -> Add)]]
-        else
-            let old = combinations n (mMuls-1)
-            let newComb = old |> List.last |> List.collect addOneMul |> List.distinct
-            old @ [newComb]
-    combinations n n
-
-let rec generateLists n =
-    if n = 0 then
-        [[]]
-    else
-        let smallerLists = generateLists (n - 1)
-        List.collect (fun lst -> [Add::lst; Mul::lst; Concat::lst]) smallerLists
+let rec generateLists (operators: Operation list) n =
+    if n = 0 then [[]] else
+        let smallerLists = generateLists operators (n - 1)
+        List.collect (fun lst -> operators |> List.map (fun op -> op::lst)) smallerLists
 
 let eval (values:int64 array) (operations:Operation list) =
     let zip = Array.zip values (List.toArray (Add::operations))
@@ -38,16 +18,10 @@ let eval (values:int64 array) (operations:Operation list) =
             | Concat -> string(acc) + string(v) |> int64
         ) 0L
 
-let isSolvable (values: int64 array) (target: int64) =
-    operatorCombinations (values.Length-1)
-    |> List.map (List.map (fun ops -> (eval values ops, Array.contains 1L values)))
-    |> List.takeWhile (List.exists (fun (ev,hasOne) -> ev <= target || hasOne))
-    |> List.map (List.map fst)
-    |> List.exists (List.exists ((=) target))
-
-let isSolvable2 (values: int64 array) (target: int64) =
-    generateLists (values.Length-1)
-    |> List.map (fun ops -> eval values ops)
+let isSolvable (operators:Operation list) (values: int64 array) (target: int64) =
+    generateLists operators (values.Length-1)
+    |> List.map (fun ops -> (eval values ops, Array.contains 1L values))
+    |> List.map fst
     |> List.exists ((=) target)
 
 let parseLine (line:string) =
@@ -61,14 +35,11 @@ let parseInput fileName =
     System.IO.File.ReadAllLines(fileName)
     |> Array.map parseLine
 
-let solvePart1 fileName =
+let calibrateWith (operators:Operation list) fileName =
     parseInput fileName
-    |> Array.filter (fun (target, values) -> isSolvable values target)
+    |> Array.filter (fun (target, values) -> isSolvable operators values target)
     |> Array.map fst
     |> Array.sum
 
-let solvePart2 fileName =
-    parseInput fileName
-    |> Array.filter (fun (target, values) -> isSolvable2 values target)
-    |> Array.map fst
-    |> Array.sum
+let solvePart1 = calibrateWith [Add;Mul]
+let solvePart2 = calibrateWith [Add;Mul;Concat]
--- AdventOfCode2024-Day07/Tests.fs
+++ AdventOfCode2024-Day07/Tests.fs
@@ -4,53 +4,31 @@ open System
 open Xunit
 open Day7
 
-[<Fact>]
-let ``My test`` () =
-    Assert.Equal(3749L, solvePart1 "Data7-Example.txt")
+let exampleFile = "Data7-Example.txt"
+let file = "Data7.txt"
 
 [<Fact>]
-let ``My testkmwalda`` () =
-    let value = solvePart1 "Data7.txt"
-    Assert.Equal(4122618559853L, value)
+let ``solvePart1 ExampleFile`` () =
+    Assert.Equal(3749L, solvePart1 exampleFile)
 
 [<Fact>]
-let ``My teawdwast`` () =
-    Assert.Equal(11387L, solvePart2 "Data7-Example.txt")
+let ``solvePart1 File`` () =
+    Assert.Equal(4122618559853L, solvePart1 file)
 
 [<Fact>]
-let ``My testawdkmwalda`` () =
-    let value = solvePart2 "Data7.txt"
-    Assert.Equal(4122618559853L, value)
+let ``solvePart2 ExampleFile`` () =
+    Assert.Equal(11387L, solvePart2 exampleFile)
 
 [<Fact>]
-let ``adwMy testa`` () =
-    let x = addOneMul [Add;Add;Add]
-    Assert.Equal(3, x.Length)
-    Assert.Equal([|Mul;Add;Add|], x[0])
-    Assert.Equal([|Add;Mul;Add|], x[1])
-    Assert.Equal([|Add;Add;Mul|], x[2])
+let ``solvePart2 File`` () =
+    Assert.Equal(227615740238334L, solvePart2 file)
 
 [<Fact>]
-let ``aval`` () =
+let ``numbers,ops -> eval -> result`` () =
     let value = eval [|2;3;4|] [Add;Mul]
     Assert.Equal((2L+3L)*4L, value)
 
 [<Fact>]
-let ``avalawd`` () =
+let ``numbers,ops -> eval -> result 2`` () =
     let value = eval [|11;6;16;20|] [Add;Mul;Add]
     Assert.Equal(292L, value)
-
-[<Fact>]
-let ``avawawdwal`` () =
-    Assert.Equal(1, [[1;2];[1;2]] |> List.distinct |> List.length)
-
-[<Fact>]
-let ``avawdal`` () =
-    let value = operatorCombinations 6
-    Assert.Equal(64, value |> List.collect id |> List.distinct |> List.length)
-    value |> List.iteri (fun i b -> Assert.True(List.forall (fun a -> a |> List.filter ((=) Mul) |> List.length |> ((=) i)) b))
-
-[<Fact>]
-let ``avawdawdal`` () =
-    let value = generateLists 3
-    Assert.Equal(27, value |> List.distinct |> List.length)
