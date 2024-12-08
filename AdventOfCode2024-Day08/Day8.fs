be8d9521a4b7d6b3149200cf0b4db68a9f8bd65f
Ian Winter <ian.winter@medicalvalues.de>
2024-12-08 18:44:27 +0100

    day8 part 2

--- AdventOfCode2024-Day08/Day8.fs
+++ AdventOfCode2024-Day08/Day8.fs
@@ -11,23 +11,40 @@ let readInput fileName =
     let height = field |> Array.map (fun (_,_,y) -> y) |> Array.max |> (+) 1
     (field, width, height)
 
-let findAntiNode (x,y) (a,b) = (x+(x-a), y+(y-b))
-
+let findAntiNode (x,y) (a,b) = [|(x+(x-a), y+(y-b))|]
+let rec gcd x y =
+    if y = 0 then abs x
+    else gcd y (x % y)
+let normalize (x,y) =
+    let divider = gcd x y
+    (x/divider, y/divider)
+let generator ((x,y), (u,v)) =
+    let newPos = (x+u,y+v)
+    Some ((x,y), (newPos, (u,v)))
+let findAntiRow (x,y) (a,b) =
+    let dir = normalize (x-a, y-b)
+    Seq.unfold generator ((a,b), dir)
 let isOnFiled width heigth (x,y) = 0 <= x && x < width && 0 <= y && y < heigth 
 
-let findAntiNodes (antennaLocations: (int*int) array) =
+let findAntiNodes antiNodeCalculator width height (antennaLocations: (int*int) array) =
     SeqU.cartesian antennaLocations antennaLocations
     |> Seq.filter (fun (loc1, loc2) -> loc1 <> loc2)
-    |> Seq.map (fun (loc1, loc2) -> findAntiNode loc1 loc2)
+    |> Seq.map (fun (loc1, loc2) -> antiNodeCalculator loc1 loc2 |> Seq.takeWhile (isOnFiled width height))
+    |> Seq.collect id
     |> Seq.toArray
 
-let solvePart1 fileName =
+let solve fileName antiNodeCalculator =
     let (field, width, height) = readInput fileName
     field
     |> Array.groupBy (fun (c,_,_) -> c)
     |> Array.filter (fst >> (<>) '.')
     |> Array.map (fun (_, g) -> g |> Array.map (fun (_,a,b) -> (a,b)))
-    |> Array.collect findAntiNodes
-    |> Array.filter (isOnFiled width height)
+    |> Array.collect (findAntiNodes antiNodeCalculator width height)
     |> Array.distinct
     |> Array.length
+
+let solvePart1 fileName =
+    solve fileName findAntiNode
+
+let solvePart2 fileName =
+    solve fileName findAntiRow
--- AdventOfCode2024-Day08/Tests.fs
+++ AdventOfCode2024-Day08/Tests.fs
@@ -15,5 +15,31 @@ let ``SolvePart1 File`` () =
     Assert.Equal(364, solvePart1 file)
 
 [<Fact>]
-let ``points -> findAntiNode -> antiNode`` () =
-    Assert.Equal((0,8), findAntiNode (0, 5) (0, 2))
+let ``SolvePart2 ExampleFile`` () =
+    Assert.Equal(34, solvePart2 exampleFile)
+
+[<Fact>]
+let ``SolvePart2 File`` () =
+    Assert.Equal(1231, solvePart2 file)
+
+module findingTests =
+
+    [<Fact>]
+    let ``points -> findAntiNode -> antiNode`` () =
+        Assert.Equal((0,8), findAntiNode (0, 5) (0, 2) |> Seq.exactlyOne)
+
+    [<Fact>]
+    let ``points -> findAntiRow -> antiRow`` () =
+        let xs = findAntiRow (0, 5) (0, 2) |> Seq.take 6 |> Seq.toArray
+        Assert.Equal((0,2), xs[0])
+        Assert.Equal((0,3), xs[1])
+        Assert.Equal((0,5), xs[3])
+        Assert.Equal((0,7), xs[5])
+
+    [<Fact>]
+    let ``points rev -> findAntiRow -> antiRow rev`` () =
+        let xs = findAntiRow (0, 2) (0, 5) |> Seq.take 6 |> Seq.toArray
+        Assert.Equal((0,5), xs[0])
+        Assert.Equal((0,4), xs[1])
+        Assert.Equal((0,2), xs[3])
+        Assert.Equal((0,0), xs[5])
