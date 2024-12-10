f373191f94589787ed87fee57f2716e1ad87a8d9
Ian Winter <ian.winter@medicalvalues.de>
2024-12-10 16:08:22 +0100

    util

--- AdventOfCode-Utils/Library.fs
+++ AdventOfCode-Utils/Library.fs
@@ -16,3 +16,26 @@ module FileU =
 
 module Array2DU =
     let isOnMap (map:'a[,]) (x,y) = x >= 0 && x < map.GetLength(0) && y >= 0 && y < map.GetLength(1)
+
+type Vector(x: int, y : int) =
+    member this.x = x
+    member this.y = y
+    static member (~-) (v : Vector) =
+        Vector(-v.x, -v.y)
+    static member (*) (v : Vector, a) =
+        Vector(a * v.x, a * v.y)
+    static member (*) (a, v: Vector) =
+        Vector(a * v.x, a * v.y)
+    static member (+) (v1 : Vector, v2 : Vector) =
+        Vector(v1.x + v2.x, v1.y + v2.y)
+    static member (-) (v1 : Vector, v2 : Vector) =
+        Vector(v1.x - v2.x, v1.y - v2.y)
+    static member from (x, y) = Vector(x, y)
+    override this.Equals (obj: obj): bool =
+        match obj with
+        | :? Vector as v -> v.x = this.x && v.y = this.y
+        | _ -> false
+    override this.GetHashCode() =
+        this.x.GetHashCode() + 7 * this.y.GetHashCode()
+    override this.ToString() =
+        this.x.ToString() + " " + this.y.ToString()
