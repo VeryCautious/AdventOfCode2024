4cea7bf3bd0bdabe3a90af58eb2cfe2fe8fb1746
Ian Winter <ian.winter@medicalvalues.de>
2024-12-13 09:23:52 +0100

    part 2

--- AdventOfCode2024-Day13/Day13.fs
+++ AdventOfCode2024-Day13/Day13.fs
@@ -3,32 +3,38 @@
 open System.Text.RegularExpressions
 open System.IO
 
-type Button = int * int
-type PrizePosition = int * int
+type Button = int64 * int64
+type PrizePosition = int64 * int64
 type Machine = Button * Button * PrizePosition
 
-let readInput fileName =
+let readInput fileName (dx,dy) =
     let btnPattern = new Regex(@"^Button .: X\+(\d+), Y\+(\d+)$")
     let prizePattern = new Regex(@"^Prize: X=(\d+), Y=(\d+)$")
     let parseButton (s:string) = 
         let m = btnPattern.Match(s)
-        (int m.Groups[1].Value, int m.Groups[2].Value)
+        (int64 m.Groups[1].Value, int64 m.Groups[2].Value)
     let parsePrize (s:string) =
         let m = prizePattern.Match(s)
-        (int m.Groups[1].Value, int m.Groups[2].Value)
+        (int64 m.Groups[1].Value |> (+) dx, int64 m.Groups[2].Value |> (+) dy)
     File.ReadAllLines(fileName)
     |> Array.chunkBySize 4
     |> Array.map (fun a -> Machine (Button (parseButton a[0]), Button (parseButton a[1]), parsePrize a[2]))
 
 let winnable (((x1, x2), (y1, y2), (c1, c2)):Machine) = 
     let det = x1*y2 - x2*y1
-    if det = 0 then None else
+    if det = 0L then None else
         let x = (c1*y2 - c2*y1)
         let y = (x1*c2 - x2*c1)
-        if x % det <> 0 || y % det <> 0 then None else Some (x / det, y / det)
+        if x % det <> 0L || y % det <> 0L then None else Some (x / det, y / det)
 
 let solvePart1 fileName = 
-    readInput fileName
+    readInput fileName (0L,0L)
     |> Array.choose winnable
-    |> Array.map (fun (x, y) -> 3 * x + y)
+    |> Array.map (fun (x, y) -> 3L * x + y)
+    |> Array.sum
+
+let solvePart2 fileName = 
+    readInput fileName (10000000000000L,10000000000000L)
+    |> Array.choose winnable
+    |> Array.map (fun (x, y) -> 3L * x + y)
     |> Array.sum
--- AdventOfCode2024-Day13/Tests.fs
+++ AdventOfCode2024-Day13/Tests.fs
@@ -8,12 +8,20 @@ let file = "Data13.txt"
 
 [<Fact>]
 let ``solvePart1 exampleFile`` () =
-    Assert.Equal(480, solvePart1 exampleFile)
+    Assert.Equal(480L, solvePart1 exampleFile)
 
 [<Fact>]
 let ``solvePart1 file`` () =
-    Assert.Equal(27105, solvePart1 file)
+    Assert.Equal(27105L, solvePart1 file)
+
+[<Fact>]
+let ``solvePart2 exampleFile`` () =
+    Assert.Equal(875318608908L, solvePart2 exampleFile)
+
+[<Fact>]
+let ``solvePart2 file`` () =
+    Assert.Equal(101726882250942L, solvePart2 file)
 
 [<Fact>]
 let ``winnable example`` () =
-    Assert.Equal(Some (80,40), winnable (Button (94,34), Button (22,67), PrizePosition (8400,5400)))
+    Assert.Equal(Some (80L,40L), winnable (Button (94L,34L), Button (22L,67L), PrizePosition (8400L,5400L)))
